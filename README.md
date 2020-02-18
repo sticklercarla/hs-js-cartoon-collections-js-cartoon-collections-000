ddd4c448e7e40ad1a691072ed6f1ac81045b9075
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-02-18 03:14:37 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Objectives
+Hello## Objectives
 
 You're going to get familiar with iterating through arrays in Javascript. 
 
--- index.js
+++ index.js
@@ -1,11 +1,80 @@
 function dwarfRollCall(dwarves) {
+  
+var string = "";
+ for(var i=0 ;i<dwarves.length; i++){
+   
+   string+= `${i+1}. ${dwarves[i]} `;
+    
+   }
+ return string;
 }
+
 
 function summonCaptainPlanet(planeteerCalls){
+  
+  
+  var array1 =[] ;
+  
+  for(var i=0; i<planeteerCalls.length; i++){
+  
+    array1.push(planeteerCalls[i].toUpperCase() + "!");
+}
+    return array1;
+
 }
 
+
+
+
 function longPlaneteerCalls(words) {
+  
+for(var i=0; i<words.length; i++){
+  
+
+  if(words[i].length >4){
+  
+    return  true;
+}
+return false;
+} 
+  
 }
 
 function findTheCheese (foods) {
+ var cheddarCheese=[banana,cheddar,socks];
+ 
+ cheddarCheese.find(element => element)
+  for(var i=0; i > foods.length; i++){
+    
+  }
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
